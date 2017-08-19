d04f5c3e50abf86f0462c5a606dab97c4b254e5e
tanjie <327886201@qq.com>
2017-08-19 16:21:26 +0800

    使用axios获取推荐模块歌单数据

--- vue-music/src/components/recommend/recommend.vue
+++ vue-music/src/components/recommend/recommend.vue
@@ -1,21 +1,33 @@
 <template>
     <div class="recommend">
-      <div class="recommend-content">
-        <!--加上v-if条件判断是否recommend已经存在，若是不存在则会影响slider的mounted状态-->
-        <div v-if="recommends.length" class="slider-wrapper">
-          <slider>
-            <div v-for="item in recommends">
-              <a :href="item.linkUrl">
-                <img :src="item.picUrl" alt="">
-              </a>
-            </div>
-          </slider>
+      <scroll ref="scroll" class="recommend-content" :data="discList">
+        <div>
+          <!--加上v-if条件判断是否recommend已经存在，若是不存在则会影响slider的mounted状态-->
+          <div v-if="recommends.length" class="slider-wrapper">
+            <slider>
+              <div v-for="item in recommends">
+                <a :href="item.linkUrl">
+                  <img :src="item.picUrl" @load="loadImage" alt="">
+                </a>
+              </div>
+            </slider>
+          </div>
+          <div class="recommend-list">
+            <h1 class="list-title">热门歌单推荐</h1>
+            <ul>
+              <li v-for="item in discList" class="item">
+                <div class="icon">
+                  <img :src="item.imgurl" alt="" width=60 height=60>
+                </div>
+                <div class="text">
+                  <h2 class="name" v-html="item.creator.name"></h2>
+                  <p class="desc" v-html="item.dissname"></p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div class="recommend-list">
-          <h1 class="list-title">热门歌单推荐</h1>
-          <ul></ul>
-        </div>
-      </div>
+      </scroll>
     </div>
 </template>
 
@@ -23,15 +35,19 @@
   import { getRecommend, getDiscList } from 'api/recommend'
   import { ERR_OK } from 'api/config'
   import Slider from 'base/slider/slider'
+  import Scroll from 'base/scroll/scroll'
 
   export default {
     data() {
       return {
-        recommends: []
+        recommends: [],
+        discList: []
       }
     },
     created() {
-      this._getRecommend()
+      setTimeout(() => {
+        this._getRecommend()
+      }, 2000)
       this._getDiscList()
     },
     methods: {
@@ -47,12 +63,21 @@
         getDiscList().then((res) => {
           if (res.code === ERR_OK) {
             console.log(res.data)
+            this.discList = res.data.list
+            console.log(this.discList)
           }
         })
+      },
+      loadImage() { // 绑定loadImage函数，确保在有图片撑开轮播图的时候，再去刷新scroll组件，并调用refresh方法 || 可以用这种方法防止任何后渲染出来的东西导致BScroll调用失败
+        if (!this.checkLoaded) {
+          this.$refs.scroll.refresh()
+          this.checkLoaded = true
+        }
       }
     },
     components: {
-      Slider
+      Slider,
+      Scroll
     }
   }
 </script>
@@ -79,4 +104,26 @@
           text-align: center
           font-size: $font-size-medium
           color: $color-theme
+        .item
+          display: flex
+          box-sizing: border-box
+          align-items: center
+          padding: 0 20px 20px 20px
+          .icon
+            flex: 0 0 60px
+            width: 60px
+            padding-right: 20px
+          .text
+            display: flex
+            flex-direction: column
+            justify-content: center
+            flex: 1
+            line-height: 20px
+            overflow: hidden
+            font-size: $font-size-medium
+            .name
+              margin-bottom: 10px
+              color: $color-text
+            .desc
+              color: $color-text-d
 </style>
