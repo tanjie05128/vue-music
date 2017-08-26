630eeaf82f0b81be2f6ad07c4a5b969f182358bd
tanjie <327886201@qq.com>
2017-08-26 15:05:44 +0800

    使用js封装对css前缀设置方法，音乐列表组件加入loading组件

--- vue-music/src/components/music-list/music-list.vue
+++ vue-music/src/components/music-list/music-list.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="music-list">
-      <div class="back">
+      <div class="back" @click="back">
         <i class="icon-back"></i>
       </div>
       <h1 class="title" v-html="title"></h1>
       <div class="bg-image" :style="bgStyle" ref="bgImage">
+        <div class="play-wrapper">
+          <div class="play" v-show="songs.length > 0" ref="playBtn">
+            <i class="icon-play"></i>
+            <span class="text">随机播放全部</span>
+          </div>
+        </div>
         <div class="filter" ref="filter"></div>
       </div>
       <div class="bg-layer" ref="layer"></div>
@@ -17,6 +23,9 @@
         <div class="song-list-wrapper">
           <song-list :songs="songs"></song-list>
         </div>
+        <div class="loading-container" v-show="!songs.length">
+          <loading></loading>
+        </div>
       </scroll>
     </div>
 </template>
@@ -24,8 +33,12 @@
 <script type="text/ecmascript-6">
   import Scroll from 'base/scroll/scroll'
   import SongList from 'base/song-list/song-list'
+  import Loading from 'base/loading/loading'
+  import {prefixStyle} from 'common/js/dom'
 
   const RESERVED_HEIGHT = 40
+  const transform = prefixStyle('transform')
+  const backdrop = prefixStyle('backdrop-filter')
 
   export default {
     props: {
@@ -64,6 +77,9 @@
     methods: {
       scroll(pos) {
         this.scrollY = pos.y
+      },
+      back() {
+        this.$router.back()
       }
     },
     watch: {
@@ -72,10 +88,7 @@
         let zIndex = 0
         let scale = 1
         let blur = 0
-//        this.$refs.layer.style['transform'] = `translate3d(0, ${newY}px, 0)`
-//        this.$refs.layer.style['webkitTransform'] = `translate3d(0, ${newY}px, 0)`
-        this.$refs.layer.style['transform'] = `translate3d(0, ${translateY}px, 0)`
-        this.$refs.layer.style['webkitTransform'] = `translate3d(0, ${translateY}px, 0)`
+        this.$refs.layer.style[transform] = `translate3d(0, ${translateY}px, 0)`
         const percent = Math.abs(newY / this.imageHeight)
         if (newY > 0) {
           scale = 1 + percent
@@ -83,24 +96,25 @@
         } else {
           blur = Math.min(20 * percent, 20)
         }
-        this.$refs.filter.style['backdrop-filter'] = `blur(${blur}px)`
-        this.$refs.filter.style['webkitBackdrop-filter'] = `blur(${blur}px)`
+        this.$refs.filter.style[backdrop] = `blur(${blur}px)` // 设置高斯模糊
         if (newY < this.minTranslateY) {
           zIndex = 10
           this.$refs.bgImage.style.paddingTop = 0
           this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+          this.$refs.playBtn.style.display = 'none'
         } else {
           this.$refs.bgImage.style.paddingTop = '70%'
           this.$refs.bgImage.style.height = 0
+          this.$refs.playBtn.style.display = ''
         }
         this.$refs.bgImage.style.zIndex = zIndex
-        this.$refs.bgImage.style['transform'] = `scale(${scale})`
-        this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})`
+        this.$refs.bgImage.style[transform] = `scale(${scale})`
       }
     },
     components: {
       Scroll,
-      SongList
+      SongList,
+      Loading
     }
   }
 </script>
@@ -125,7 +139,7 @@
       .icon-back
         display: block
         padding: 10px
-        font-size: $font-size-large-x
+        font-size: $font-size-large
         color: $color-theme
     .title
       position: absolute
